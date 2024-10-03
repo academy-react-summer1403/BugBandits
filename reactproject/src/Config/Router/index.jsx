c23f7164cc72e36fa2d3e8c5ed6cba54be2a717e
sahar <148485368+saharsadeghi99@users.noreply.github.com>
2024-10-03 12:07:01 +0330

    Revert "Develop"

--- reactproject/src/Config/Router/index.jsx
+++ reactproject/src/Config/Router/index.jsx
@@ -2,20 +2,20 @@ import React from "react"
 import { createBrowserRouter } from "react-router-dom"
 import MainLayout from "./MainLayout"
 import LandingHolder from "../../Components/Landing/LandingHolder"
-import { Blog } from "../../Components/Blog"
+
 
 export const router = createBrowserRouter([
     {
         path:"/",
         element:<MainLayout />,
-        children:[[
+        children:[
+            [
                 {
                     path:"/",
                     element:<LandingHolder />
                 },
-               
                 
-                
-        ]]
+            ]
+        ]
     }
 ])
--- reactproject/tailwind.config.js
+++ reactproject/tailwind.config.js
@@ -5,23 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        
-        'light_gray': '#c3c3c3', // search palce holder
-        'dark_gray': '#707070',
-        'silver_gray': '#bfbfbf',
-        'charcoal_gray': '#555555', // blog title
-        'medium_gray': '#a7a7a7', // landing des
-        'soft_grey' : '#F3F4F6' , //landing bg
-        
-        'bright_blue': '#267dff',
-        'ocean_blue': '#436e8e', // bold titles
-        'dusty_blue': '#8c9aac', // card desc
-        'cool_blue': '#475466', // card title
-
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
