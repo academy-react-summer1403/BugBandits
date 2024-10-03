cdf64cddf757051332306a99936a7d3b3daa7173
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-10-03 16:22:56 +0330

    img edit

--- reactproject/src/Components/Common/Header/index.jsx
+++ reactproject/src/Components/Common/Header/index.jsx
@@ -9,17 +9,16 @@ const Header = () => {
         <Fragment>
         <header dir="rtl" className="w-5/6 h-20 borer m-auto pt-10 flex flex-row ">
           <div className="flex text-dark_gray ml-[780px]">
-              <NavLink to="" className="ml-7">logo</NavLink>
-              <NavLink to="" className="ml-7">خانه</NavLink>
-              <NavLink to="" className="ml-7">دوره ها</NavLink>
-              <NavLink to="" className="ml-7">اساتید</NavLink>
-              <NavLink to="" className="ml-7">اخبار</NavLink>
-              <NavLink to="" className="ml-7">درباره ما</NavLink>
+              <NavLink to="" className="ml-7 hover:text-bright_blue">logo</NavLink>
+              <NavLink to="" className="ml-7 hover:text-bright_blue">خانه</NavLink>
+              <NavLink to="" className="ml-7 hover:text-bright_blue">دوره ها</NavLink>
+              <NavLink to="" className="ml-7 hover:text-bright_blue">اساتید</NavLink>
+              <NavLink to="" className="ml-7 hover:text-bright_blue">اخبار</NavLink>
+              <NavLink to="" className="ml-7 hover:text-bright_blue">درباره ما</NavLink>
           </div>
           <div className="flex">
               <NavLink><HiMoon className="w-9 h-9 text-white bg-ocean_blue rounded-full border-4 border-ocean_blue" /></NavLink>
               <NavLink><FaUserCircle className="w-9 h-9 text-ocean_blue mr-2" /></NavLink>
-  
           </div>
         </header>
       </Fragment>
--- reactproject/src/Components/Landing/HeroSection/index.jsx
+++ reactproject/src/Components/Landing/HeroSection/index.jsx
@@ -1,24 +1,29 @@
 import React, { Fragment } from "react";
 import HeroSectionimg01 from "./../../../assets/images/herosection01.svg";
-import HeroSectionimg02 from "./../../../assets/images/herosection02.svg";
+import HeroSectionimg03 from "./../../../assets/images/herosection03.svg";
+
 import bgdesign from "./../../../assets/images/bgdesign.svg";
+import { LuSearch } from "react-icons/lu";
 
 const HeroSection = () => {
   return (
     <Fragment>
-      <img src={bgdesign} className="w-[900px] h-[900px] absolute top-1 right-0 -z-10" />
-      <div className="border w-11/12 h-[600px] m-auto mt-40 flex relative ">
+      <img
+        src={bgdesign}
+        className="w-[900px] h-[900px] absolute top-1 right-0 -z-10"
+      />
+      <div className="border w-11/12 h-[600px] m-auto mt-20 flex relative ">
         <div>
           <img src={HeroSectionimg01} alt="not found" />
           <img
-            src={HeroSectionimg02}
+            src={HeroSectionimg03}
             alt="not found"
             className="absolute bottom-[400px] left-12 "
           />
           <h1></h1>
         </div>
         <div dir="rtl">
-          <div className="border ">
+          <div className="border pt-24">
             <h1 className="text-ocean_blue text-4xl font-bold mb-3">
               دنبال چی می گردی؟
             </h1>
@@ -27,16 +32,21 @@ const HeroSection = () => {
             </h4>
           </div>
 
-          <div className="border">
-            <input className="w-72 h-8 border border-ocean_blue rounded-lg" />
-            <button></button>
+          <div className="border pt-1">
+            <input
+              className="w-80 h-12 border pr-5 pl-12 border-ocean_blue rounded-2xl placeholder:text-light_gray placeholder:text-sm outline-none text-dark_gray"
+              placeholder="تو فقط اسم ببر ... "
+            />
+            <button className=" relative top-[0.7rem] left-[2.86rem]">
+              <LuSearch className="w-9 h-9 text-white bg-ocean_blue rounded-xl border-4 border-ocean_blue"/>
+            </button>
           </div>
 
           <div className="border">
-            <h2>آکادمی سپهر</h2>
-            <h5>
-              آکاپمی آموزشی سپهر مکانی برای پیشرفت تو تا بتونی مهارت های مورد
-              نیزار برای طراحی وب رو هرچه سریعتر و با بهترین روش یاد بگیری
+            <h2 className="text-4xl text-charcoal_gray">آکادمی سپهر</h2>
+            <h5 className="text-base text-medium_gray pt-3">
+              آکادمی آموزشی سپهر مکانی برای پیشرفت <br />تو تا بتونی مهارت های مورد
+              نیاز برای طراحی وب رو <br />هرچه سریعتر و با بهترین روش یاد بگیری
             </h5>
           </div>
         </div>
