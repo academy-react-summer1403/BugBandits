03c6294c0b5b69ce7f043543693d2e76187f83de
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-10-03 21:56:12 +0330

    accept develop changes

--- reactproject/src/Config/Router/index.jsx
+++ reactproject/src/Config/Router/index.jsx
@@ -1,27 +1,18 @@
-import React from "react"
-import { createBrowserRouter } from "react-router-dom"
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./../../app/MainLayout";
-import {LandingHolder} from "../../Components/Landing/LandingHolder"
+import { LandingHolder } from "../../Components/Landing/LandingHolder";
 
 export const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<MainLayout />,
-        children:[
-                {
-                    index:true ,
-                    path:"/",
-<<<<<<< HEAD
-                    element:<LandingHolder />,
-                },
-=======
-                    element:<LandingHolder />
-                },
-               
-                
->>>>>>> Develop
-                
-                
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        path: "/",
+        element: <LandingHolder />,
+      },
+    ],
+  },
+]);
--- reactproject/src/main.jsx
+++ reactproject/src/main.jsx
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {App} from './app'
-import './index.css'
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import {App} from "./app";
 import "./index.css";
 import "./assets/Fonts/fonts.css";
->>>>>>> Develop
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
