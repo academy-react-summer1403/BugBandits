5ef98c66ede87d3d964ad069fe99b441fc89f0f9
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-10-06 06:41:29 +0330

    register

--- reactproject/src/app/LogLayout/index.jsx
+++ reactproject/src/app/LogLayout/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Log } from '../../Components/Common/Register&Login';
+import { Outlet } from 'react-router-dom';
 
 const LogLayout = () => {
     return (
         <>
             <Log/>
+            <Outlet/>
         </>
     );
 }
