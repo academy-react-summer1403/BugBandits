757779e200b2a578fc0bc2d4289fa933c645f322
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-10-09 23:38:23 +0330

    interceptor update

--- reactproject/src/Services/interceptor/index.jsx
+++ reactproject/src/Services/interceptor/index.jsx
@@ -1,7 +1,15 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_BASE_URL
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 const instance = axios.create({
-    baseURL:baseURL
-})
+  baseURL: baseURL,
+});
+
+const onSuccess = (response) => {
+  return response.data;
+};
+const onError = (err) => {};
+instance.interceptors.response.use(onSuccess, onError);
+
+export default instance;
