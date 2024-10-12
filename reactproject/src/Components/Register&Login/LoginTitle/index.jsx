2cffb1bfad3956432458b95f3a88527e3abcfec6
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-10-12 12:33:58 +0330

    dark mode

--- reactproject/src/Components/Register&Login/LoginTitle/index.jsx
+++ reactproject/src/Components/Register&Login/LoginTitle/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 const LoginTitle = ({log,text}) => {
+    const darkMode = useSelector((state) => state.darkMode.value);
     return (
-        <div>
+        <div className={`${darkMode ? "dark" : ""}`}>
             <div>
             <span className="w-20 h-16 inline-block">logo</span>
-            <span className="inline-block align-middle text-charcoal_gray text-4xl font-bold">آکادمی سپهر</span>
+            <span className="inline-block align-middle text-charcoal_gray text-4xl font-bold dark:text-ocean_blue">آکادمی سپهر</span>
         </div>
         <div className="mt-5">
-            <h2 className="text-charcoal_gray text-xl font-bold pb-2"> ورود با موبایل</h2>
-            <span className="text-charcoal_gray pl-2 text-sm">{text}</span>
+            <h2 className="text-charcoal_gray text-xl font-bold pb-2 dark:text-white"> ورود با موبایل</h2>
+            <span className="text-charcoal_gray pl-2 text-sm dark:text-white">{text}</span>
             <NavLink to="/register" className="text-ocean_blue text-sm">{log}</NavLink>
         </div>
         </div>
--- reactproject/src/Redux/Slices/DarkModeSlice.js
+++ reactproject/src/Redux/Slices/DarkModeSlice.js
@@ -4,11 +4,12 @@ import { createSlice } from "@reduxjs/toolkit";
 export const DarkModeSlice = createSlice({
     name:"darkmode",
     initialState:{
-        value:false
+        value: localStorage.getItem("darkMode") === "true"
     },
     reducers:{
         toggleDarkMode:(state,action)=>{
-            state.value=!state.value
+            state.value=!state.value;
+            localStorage.setItem("darkMode", state.value);
         }
     }
 })
