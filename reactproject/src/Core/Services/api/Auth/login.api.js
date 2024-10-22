7cdcd807204d1b6e2ed54eddee5cd43ba0ac898d
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-10-22 23:04:58 +0330

    api

---
+++ reactproject/src/Core/Services/api/Auth/login.api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const Postlogin = async (user) => {
+  try {
+    const response = await axios.post(
+      "https://classapi.sepehracademy.ir/api/Sign/Login",
+      user
+    );
+    return response.data;
+  } catch (error) {
+    return [];
+  }
+};
