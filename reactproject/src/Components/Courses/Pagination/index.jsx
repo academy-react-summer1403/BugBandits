034739abc1380231131b7fca64e355558b64e07d
sabaw02 <133888147+sabaw02@users.noreply.github.com>
2024-10-28 20:33:08 +0330

    pagination

--- reactproject/src/Components/Courses/Pagination/index.jsx
+++ reactproject/src/Components/Courses/Pagination/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Button, IconButton } from "@material-tailwind/react";
+import { BsArrowRight } from "react-icons/bs";
+import { BsArrowLeft } from "react-icons/bs";
 
 const DefaultPagination = ({ courseList, getData }) => {
   const numberOfPage = courseList && Math.ceil(courseList.totalCount / 9);
@@ -11,53 +13,56 @@ const DefaultPagination = ({ courseList, getData }) => {
   }
   const [active, setActive] = useState(1);
 
-  const getItemProps = (index) => {
-    return {
-      variant: active === index ? "filled" : "text",
-      color: "gray",
-      onClick: () => setActive(index),
-    };
+  const handlePageChange = (index) => {
+    setActive(index);
+    getData(index);
   };
 
   const next = () => {
-    if (active < 5) {
-      setActive(active + 1);
+    if (active < numberOfPage) {
+      handlePageChange(active + 1);
     }
   };
 
   const prev = () => {
     if (active > 1) {
-      setActive(active - 1);
+      handlePageChange(active - 1);
     }
   };
 
   return (
-    <div className="flex items-center gap-4">
-      <Button
-        variant="text"
-        className="flex items-center gap-2"
-        onClick={prev}
-        disabled={active === 1}
-      ></Button>
-      <div className="flex items-center gap-2">
-        {numberOfPageArr?.map((number) => (
-          <IconButton
-            key={number}
-            {...getItemProps(number)}
-            onClick={() => getData(number)}
-          >
-            {number}
-          </IconButton>
-        ))}
+    <div className="flex flex-col items-center gap-4">
+      <div className="flex flex-row">
+        <Button
+          variant="text"
+          className="flex items-center gap-2"
+          onClick={prev}
+          disabled={active === 1}
+        >
+          <BsArrowRight className="dark:text-white"/>
+        </Button>
+        <div className="flex items-center gap-2">
+          {numberOfPageArr?.map((number) => (
+            <IconButton
+              key={number}
+              variant={active === number ? "filled" : "text"}
+              // color="gray"
+              onClick={() => handlePageChange(number)}
+              className=" font-kalamehNum dark:text-white"
+            >
+              {number}
+            </IconButton>
+          ))}
+        </div>
+        <Button
+          variant="text"
+          className="flex items-center gap-2"
+          onClick={next}
+          disabled={active === 10}
+        >
+          <BsArrowLeft className="dark:text-white"/>
+        </Button>
       </div>
-      {/* <Button
-        variant="text"
-        className="flex items-center gap-2"
-        onClick={next}
-        disabled={active === 5}
-      >
-        Next
-      </Button> */}
     </div>
   );
 };
